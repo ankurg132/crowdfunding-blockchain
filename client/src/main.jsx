2d4cde0cfe872c363c6fc8bedbf0a039159f7c23
Ankur Gupta <ankur.gupta@internshala.com>
2024-05-12 02:21:19 +0530

    Initial:

---
+++ client/src/main.jsx
@@ -0,0 +1,19 @@
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ThirdwebProvider } from '@thirdweb-dev/react';
+
+import {StateContextProvider} from "./context";
+import App from './App';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <ThirdwebProvider activeChain="sepolia" clientId="85a1cb3dc4d48eb7ce5b4fea0045fdb3">
+        <Router>
+            <StateContextProvider>
+                <App />
+            </StateContextProvider>
+        </Router>
+    </ThirdwebProvider>
+)
